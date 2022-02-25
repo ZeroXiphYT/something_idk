f98dbe4166fd49df98fa7a08adfac1dba2a34015
ZeroXiphYT <vachanbharucha11@gmail.com>
2022-02-25 19:13:52 +0000

    Added This

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-var count = 0;
-function dosomething(el) {
-    el.innerText = "You Clicked The Button!";
-    count = count + 1;
-    if (count === 30) {
-        var element = document.createElement('p');
-        element.innerText = "YOU FOUND THE SECRET!!!!!!!";
-        document.body.appendChild(element);
-        create('button', 'Click For Your Prize', document.body, 'id', 'prz')
-    }
-}
-
-function create(el, text, parent, attr, attrval) {
-    var element = document.createElement(el);
-    element.innerText = text;
-    element.setAttribute(attr, attrval);
-    parent.appendChild(element);
-}
-
-var prz = document.getElementById('prz')
-prz.addEventListener('click', click)
-
-function click(){
-    window.open('https://therickroll.com', '_blank');
+function start(){
+    swal({
+        title:"You Clicked The Button",
+        text: "You Clicked The Button, That's Pretty Cool",
+        icon:"success",
+        buttons:["I Don't Care", "Ok, Thanks"]
+    })
 
 }
+
+var btn = document.getElementById('btn')
+btn.addEventListener('click', start)
